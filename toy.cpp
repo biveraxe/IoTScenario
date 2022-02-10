3b0b83e9aede11888903865daa2cd1d89ff84e4b
biver <batata@mail.ru>
2022-02-10 11:27:11 +0300

    Оборачиваем в отдельный класс

--- toy.cpp
+++ toy.cpp
@@ -11,87 +11,6 @@
 
 IoTItem* tmpItem = new IoTItem("");  //заглушка для интеграции и создания полной картины структуры
 
-std::ifstream myfile("d:\\IoTScenario\\scenario.txt");
-
-//===----------------------------------------------------------------------===//
-// Lexer (Лексический анализатор)
-//===----------------------------------------------------------------------===//
-
-// Лексический анализатор возвращает токены [0-255], если это неизвестны, 
-// иначе одну из известных единиц кода
-enum Token {
-  tok_eof = -1,
-
-  // операнды (первичные выражения: идентификаторы, числа)
-  tok_identifier = -4, tok_number = -5, tok_string = -3,
-
-  // управление
-  tok_if = -6, tok_then = -7, tok_else = -8
-};
-
-static std::string IdentifierStr;  // Заполняется, если tok_identifier
-static float NumVal;              // Заполняется, если tok_number
-
-/// gettok - Возвращает следующий токен из стандартного потока ввода.
-static int gettok() {
-  static int LastChar = ' ';
-
-  // Пропускаем пробелы.
-  while (isspace(LastChar))
-    LastChar = myfile.get();
-
-  if (isalpha(LastChar)) { // идентификатор: [a-zA-Z][a-zA-Z0-9]*
-    IdentifierStr = LastChar;
-    while (isalnum((LastChar = myfile.get())))
-      IdentifierStr += LastChar;
-
-    if (IdentifierStr == "if") return tok_if;
-    if (IdentifierStr == "then") return tok_then;
-    if (IdentifierStr == "else") return tok_else;
-    return tok_identifier;
-  }
-
-  if (isdigit(LastChar) || LastChar == '.') {   // Число: [0-9.]+
-    std::string NumStr;
-    do {
-      NumStr += LastChar;
-      LastChar = myfile.get();
-    } while (isdigit(LastChar) || LastChar == '.');
-
-    NumVal = strtod(NumStr.c_str(), 0);
-    return tok_number;
-  }
-
-  if (LastChar == '#') {
-    // Комментарий до конца строки
-    do LastChar = myfile.get();
-    while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
-    
-    if (LastChar != EOF)
-      return gettok();
-  }
-
-  if (LastChar == '"') { // "строка"    
-    IdentifierStr = "";
-    LastChar = myfile.get();
-    while (LastChar != '"') {
-      IdentifierStr += LastChar;
-      LastChar = myfile.get();
-    }
-    LastChar = myfile.get();
-      
-    return tok_string;
-  }
-  
-  // Проверка конца файла.
-  if (LastChar == EOF)
-    return tok_eof;
-
-  // В противном случае просто возвращаем символ как значение ASCII
-  int ThisChar = LastChar;
-  LastChar = myfile.get();
-  return ThisChar;
-}
 
 //===----------------------------------------------------------------------===//
 // Abstract Syntax Tree (Абстрактное Синтаксическое Дерево или Дерево Парсинга)
@@ -203,7 +122,7 @@ public:
   }
 };
 
-/// CallExprAST - Класс узла выражения для вызова функции.
+/// CallExprAST - Класс узла выражения для вызова команды.
 class CallExprAST : public ExprAST {
   std::string Callee;
   std::vector<ExprAST*> Args;
@@ -216,270 +135,343 @@ public:
   }
 };
 
-/// CmdBlockExprAST - Класс узла блока команд {..}.
-class CmdBlockExprAST : public ExprAST {
-  std::string Callee;
-  std::vector<ExprAST*> Args;
-public:
-  CmdBlockExprAST(const std::string &callee, std::vector<ExprAST*> &args)
-    : Callee(callee), Args(args) {}
-
-  IoTValue* exec() {
-    fprintf(stderr, "Call from  CallExprAST\n");
-  }
-};
-
 /// IfExprAST - Класс узла выражения для if/then/else.
 class IfExprAST : public ExprAST {
   ExprAST *Cond, *Then, *Else;
-  IoTValue* cond_ret;
+
 public:
   IfExprAST(ExprAST *cond, ExprAST *then, ExprAST *_else)
     : Cond(cond), Then(then), Else(_else) {}
 
   IoTValue* exec() {
-    IoTValue *then_ret, *else_ret;
+    IoTValue *res_ret;
+    IoTValue *cond_ret;
     cond_ret = Cond->exec();
-    if (cond_ret != nullptr && cond_ret->isDecimal && cond_ret->valD) then_ret = Then->exec();
-    else if (Else) else_ret = Else->exec();
+    if (cond_ret != nullptr && cond_ret->isDecimal && cond_ret->valD) res_ret = Then->exec();
+    else if (Else) res_ret = Else->exec();
 
-    fprintf(stderr, "Call from  IfExprAST: Cond result = %d, then result = %f, else result = %f\n", cond_ret->valD, then_ret->valD, else_ret->valD);
+    if (res_ret->isDecimal)
+      fprintf(stderr, "Call from  IfExprAST: Cond result = %f, result = %f\n", cond_ret->valD, res_ret->valD);
+    else fprintf(stderr, "Call from  IfExprAST: Cond result = %f, result = %s\n", cond_ret->valD, res_ret->valS.c_str());
     return cond_ret;
   }
 };
 
-//===----------------------------------------------------------------------===//
-// Parser (Парсер или Синтаксический Анализатор)
-//===----------------------------------------------------------------------===//
 
+class IoTScenario {
 
-/// CurTok/getNextToken - Предоставляет простой буфер токенов. CurTok - это текущий
-/// токен, просматриваемый парсером. getNextToken получает следующий токен от
-/// лексического анализатора и обновляет CurTok.
-static int CurTok;
-static int getNextToken() {
-  return CurTok = gettok();
-}
+  //===----------------------------------------------------------------------===//
+  // Lexer (Лексический анализатор)
+  //===----------------------------------------------------------------------===//
 
-/// BinopPrecedence - Содержит приоритеты для бинарных операторов
-static std::map<char, int> BinopPrecedence;
+  // Лексический анализатор возвращает токены [0-255], если это неизвестны, 
+  // иначе одну из известных единиц кода
+  enum Token {
+    tok_eof = -1,
 
-/// GetTokPrecedence - Возвращает приоритет текущего бинарного оператора.
-static int GetTokPrecedence() {
-  if (!isascii(CurTok))
-    return -1;
-  
-  // Удостоверимся, что это объявленный бинарный оператор.
-  int TokPrec = BinopPrecedence[CurTok];
-  if (TokPrec <= 0) return -1;
-  return TokPrec;
-}
+    // операнды (первичные выражения: идентификаторы, числа)
+    tok_identifier = -4, tok_number = -5, tok_string = -3,
 
-/// Error* - Это небольшие вспомогательные функции для обработки ошибок.
-ExprAST *Error(const char *Str) { fprintf(stderr, "Error: %s\n", Str);return 0;}
+    // управление
+    tok_if = -6, tok_then = -7, tok_else = -8
+  };
 
-static ExprAST *ParseExpression();
+  std::string IdentifierStr;  // Заполняется, если tok_identifier
+  float NumVal;              // Заполняется, если tok_number
 
-/// identifierexpr
-///   ::= identifier
-///   ::= identifier '(' expression* ')'
-static ExprAST *ParseIdentifierExpr() {
-  std::string IdName = IdentifierStr;
-  
-  getNextToken();  // получаем идентификатор.
-  
-  if (CurTok != '(') // Обычная переменная.
-    return new VariableExprAST(IdName, tmpItem);
-  
-  // Вызов функции.
-  getNextToken();  // получаем (
-  std::vector<ExprAST*> Args;
-  if (CurTok != ')') {
-    while (1) {
-      ExprAST *Arg = ParseExpression();
-      if (!Arg) return 0;
-      Args.push_back(Arg);
+  /// gettok - Возвращает следующий токен из стандартного потока ввода.
+  int gettok() {
+    static int LastChar = ' ';
 
-      if (CurTok == ')') break;
+    // Пропускаем пробелы.
+    while (isspace(LastChar))
+      LastChar = myfile.get();
 
-      if (CurTok != ',')
-        return Error("Expected ')' or ',' in argument list");
-      getNextToken();
+    if (isalpha(LastChar)) { // идентификатор: [a-zA-Z][a-zA-Z0-9]*
+      IdentifierStr = LastChar;
+      while (isalnum((LastChar = myfile.get())))
+        IdentifierStr += LastChar;
+
+      if (IdentifierStr == "if") return tok_if;
+      if (IdentifierStr == "then") return tok_then;
+      if (IdentifierStr == "else") return tok_else;
+      return tok_identifier;
     }
-  }
 
-  // Получаем ')'.
-  getNextToken();
-  
-  return new CallExprAST(IdName, Args);
-}
+    if (isdigit(LastChar) || LastChar == '.') {   // Число: [0-9.]+
+      std::string NumStr;
+      do {
+        NumStr += LastChar;
+        LastChar = myfile.get();
+      } while (isdigit(LastChar) || LastChar == '.');
 
-/// numberexpr ::= number
-static ExprAST *ParseNumberExpr() {
-  ExprAST *Result = new NumberExprAST(NumVal);
-  getNextToken(); // получаем число
-  return Result;
-}
+      NumVal = strtod(NumStr.c_str(), 0);
+      return tok_number;
+    }
 
-/// parenexpr ::= '(' expression ')'
-static ExprAST *ParseParenExpr() {
-  getNextToken();  // получаем (.
-  ExprAST *V = ParseExpression();
-  if (!V) return 0;
-  
-  if (CurTok != ')')
-    return Error("expected ')'");
-  getNextToken();  // получаем ).
-  return V;
-}
+    if (LastChar == '#') {
+      // Комментарий до конца строки
+      do LastChar = myfile.get();
+      while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+      
+      if (LastChar != EOF)
+        return gettok();
+    }
 
-/// quotesexpr ::= '"' expression '"'
-static ExprAST *ParseQuotesExpr() {
-  std::string StringCont = IdentifierStr;
-  ExprAST *Result = new StringExprAST(StringCont);
-  getNextToken(); // получаем число
-  return Result;
-}
-
-/// ifexpr ::= 'if' expression 'then' expression 'else' expression
-static ExprAST *ParseIfExpr() {
-  getNextToken();  // Получаем if.
-  
-  // условие.
-  ExprAST *Cond = ParseExpression();
-  if (!Cond) return 0;
-  
-  if (CurTok != tok_then)
-    return Error("expected then");
-  getNextToken();  // Получаем then
-  
-  ExprAST *Then = ParseExpression();
-  if (Then == 0) return 0;
-  
-  if (CurTok != tok_else)
-    return Error("expected else");
-  
-  getNextToken();
-  
-  ExprAST *Else = ParseExpression();
-  if (!Else) return 0;
-  
-  return new IfExprAST(Cond, Then, Else);
-}
-
-/// primary
-///   ::= identifierexpr
-///   ::= numberexpr
-///   ::= parenexpr
-static ExprAST *ParsePrimary() {
-  switch (CurTok) {
-  default: return Error("unknown token when expecting an expression");
-  case tok_identifier: return ParseIdentifierExpr();
-  case tok_number:     return ParseNumberExpr();
-  case '(':            return ParseParenExpr();
-  case tok_string:     return ParseQuotesExpr();
-  case tok_if:         return ParseIfExpr();
-  }
-}
-
-/// binoprhs
-///   ::= ('+' primary)*
-static ExprAST *ParseBinOpRHS(int ExprPrec, ExprAST *LHS) {
-  // Если это бинарный оператор, получаем его приоритет
-  while (1) {
-    int TokPrec = GetTokPrecedence();
-    
-    // Если этот бинарный оператор связывает выражения по крайней мере так же, 
-    // как текущий, то используем его
-    if (TokPrec < ExprPrec)
-      return LHS;
-    
-    // Отлично, мы знаем, что это бинарный оператор.
-    int BinOp = CurTok;
-    getNextToken();  // eat binop
-    
-    // Разобрать первичное выражение после бинарного оператора
-    ExprAST *RHS = ParsePrimary();
-    if (!RHS) return 0;
-    
-    // Если BinOp связан с RHS меньшим приоритетом, чем оператор после RHS, 
-    // то берём часть вместе с RHS как LHS.
-    int NextPrec = GetTokPrecedence();
-    if (TokPrec < NextPrec) {
-      RHS = ParseBinOpRHS(TokPrec+1, RHS);
-      if (RHS == 0) return 0;
+    if (LastChar == '"') { // "строка"    
+      IdentifierStr = "";
+      LastChar = myfile.get();
+      while (LastChar != '"') {
+        IdentifierStr += LastChar;
+        LastChar = myfile.get();
+      }
+      LastChar = myfile.get();
+        
+      return tok_string;
     }
     
-    // Собираем LHS/RHS.
-    LHS = new BinaryExprAST(BinOp, LHS, RHS);
+    // Проверка конца файла.
+    if (LastChar == EOF)
+      return tok_eof;
+
+    // В противном случае просто возвращаем символ как значение ASCII
+    int ThisChar = LastChar;
+    LastChar = myfile.get();
+    return ThisChar;
   }
-}
 
 
-/// expression
-///   ::= primary binoprhs
-///
-static ExprAST *ParseExpression() {
-  ExprAST *LHS = ParsePrimary();
-  if (!LHS) return 0;
-  
-  return ParseBinOpRHS(0, LHS);
-}
+  //===----------------------------------------------------------------------===//
+  // Parser (Парсер или Синтаксический Анализатор)
+  //===----------------------------------------------------------------------===//
 
 
+  /// CurTok/getNextToken - Предоставляет простой буфер токенов. CurTok - это текущий
+  /// токен, просматриваемый парсером. getNextToken получает следующий токен от
+  /// лексического анализатора и обновляет CurTok.
+  int CurTok;
+  int getNextToken() {
+    return CurTok = gettok();
+  }
 
-//===----------------------------------------------------------------------===//
-// Top-Level parsing (Парсинг верхнего уровня)
-//===----------------------------------------------------------------------===//
+  /// BinopPrecedence - Содержит приоритеты для бинарных операторов
+  std::map<char, int> BinopPrecedence;
 
-static void HandleIf() {
-  if (ParseIfExpr()) {
+  /// GetTokPrecedence - Возвращает приоритет текущего бинарного оператора.
+  int GetTokPrecedence() {
+    if (!isascii(CurTok))
+      return -1;
     
-  } else {
-    // Пропускаем токен для восстановления после ошибки.
+    // Удостоверимся, что это объявленный бинарный оператор.
+    int TokPrec = BinopPrecedence[CurTok];
+    if (TokPrec <= 0) return -1;
+    return TokPrec;
+  }
+
+  /// Error* - Это небольшие вспомогательные функции для обработки ошибок.
+  ExprAST *Error(const char *Str) { fprintf(stderr, "Error: %s\n", Str);return 0;}
+
+
+  /// identifierexpr
+  ///   ::= identifier
+  ///   ::= identifier '(' expression* ')'
+  ExprAST *ParseIdentifierExpr() {
+    std::string IdName = IdentifierStr;
+    
+    getNextToken();  // получаем идентификатор.
+    
+    if (CurTok != '(') // Обычная переменная.
+      return new VariableExprAST(IdName, tmpItem);
+    
+    // Вызов функции.
+    getNextToken();  // получаем (
+    std::vector<ExprAST*> Args;
+    if (CurTok != ')') {
+      while (1) {
+        ExprAST *Arg = ParseExpression();
+        if (!Arg) return 0;
+        Args.push_back(Arg);
+
+        if (CurTok == ')') break;
+
+        if (CurTok != ',')
+          return Error("Expected ')' or ',' in argument list");
+        getNextToken();
+      }
+    }
+
+    // Получаем ')'.
     getNextToken();
+    
+    return new CallExprAST(IdName, Args);
   }
-}
 
+  /// numberexpr ::= number
+  ExprAST *ParseNumberExpr() {
+    ExprAST *Result = new NumberExprAST(NumVal);
+    getNextToken(); // получаем число
+    return Result;
+  }
+
+  /// parenexpr ::= '(' expression ')'
+  ExprAST *ParseParenExpr() {
+    getNextToken();  // получаем (.
+    ExprAST *V = ParseExpression();
+    if (!V) return 0;
+    
+    if (CurTok != ')')
+      return Error("expected ')'");
+    getNextToken();  // получаем ).
+    return V;
+  }
+
+  /// quotesexpr ::= '"' expression '"'
+  ExprAST *ParseQuotesExpr() {
+    std::string StringCont = IdentifierStr;
+    ExprAST *Result = new StringExprAST(StringCont);
+    getNextToken(); // получаем число
+    return Result;
+  }
+
+  /// ifexpr ::= 'if' expression 'then' expression 'else' expression
+  ExprAST *ParseIfExpr() {
+    getNextToken();  // Получаем if.
+    
+    // условие.
+    ExprAST *Cond = ParseExpression();
+    if (!Cond) return 0;
+    
+    if (CurTok != tok_then)
+      return Error("expected then");
+    getNextToken();  // Получаем then
+    
+    ExprAST *Then = ParseExpression();
+    if (Then == 0) return 0;
+    
+    if (CurTok != tok_else)
+      return Error("expected else");
+    
+    getNextToken();
+    
+    ExprAST *Else = ParseExpression();
+    if (!Else) return 0;
+    
+    return new IfExprAST(Cond, Then, Else);
+  }
+
+  /// primary
+  ///   ::= identifierexpr
+  ///   ::= numberexpr
+  ///   ::= parenexpr
+  ExprAST *ParsePrimary() {
+    switch (CurTok) {
+    default: return Error("unknown token when expecting an expression");
+    case tok_identifier: return ParseIdentifierExpr();
+    case tok_number:     return ParseNumberExpr();
+    case '(':            return ParseParenExpr();
+    case tok_string:     return ParseQuotesExpr();
+    case tok_if:         return ParseIfExpr();
+    }
+  }
+
+  /// binoprhs
+  ///   ::= ('+' primary)*
+  ExprAST *ParseBinOpRHS(int ExprPrec, ExprAST *LHS) {
+    // Если это бинарный оператор, получаем его приоритет
+    while (1) {
+      int TokPrec = GetTokPrecedence();
+      
+      // Если этот бинарный оператор связывает выражения по крайней мере так же, 
+      // как текущий, то используем его
+      if (TokPrec < ExprPrec)
+        return LHS;
+      
+      // Отлично, мы знаем, что это бинарный оператор.
+      int BinOp = CurTok;
+      getNextToken();  // eat binop
+      
+      // Разобрать первичное выражение после бинарного оператора
+      ExprAST *RHS = ParsePrimary();
+      if (!RHS) return 0;
+      
+      // Если BinOp связан с RHS меньшим приоритетом, чем оператор после RHS, 
+      // то берём часть вместе с RHS как LHS.
+      int NextPrec = GetTokPrecedence();
+      if (TokPrec < NextPrec) {
+        RHS = ParseBinOpRHS(TokPrec+1, RHS);
+        if (RHS == 0) return 0;
+      }
+      
+      // Собираем LHS/RHS.
+      LHS = new BinaryExprAST(BinOp, LHS, RHS);
+    }
+  }
+
+
+  /// expression
+  ///   ::= primary binoprhs
+  ///
+  ExprAST *ParseExpression() {
+    ExprAST *LHS = ParsePrimary();
+    if (!LHS) return 0;
+    
+    return ParseBinOpRHS(0, LHS);
+  }
+
+  std::vector<ExprAST*> ScenarioElements;  // корневые элементы дерава   
+  std::ifstream myfile;
+
+public:
+  void loadScenario(std::string fileName) {  // посимвольно считываем и сразу интерпретируем сценарий в дерево AST
+    myfile.open(fileName);
+    if (myfile.is_open()) {
+      getNextToken();
+      while ( myfile ) {
+        switch (CurTok) {
+          case tok_eof:    return;
+          //case ';':        getNextToken(); break;  // игнорируем верхнеуровневые точки с запятой.
+          case tok_if:     ScenarioElements.push_back(ParseExpression()); break;
+          default:         getNextToken(); break;
+        }
+      }
+      myfile.close();
+    }
+  }
+
+  void ExecScenario() {  // запускаем поочереди все корневые элементы выражений в сценарии, ожидаемо - это IFы
+    for (unsigned int i = 0; i < ScenarioElements.size(); i++) {
+      if (ScenarioElements[i]) ScenarioElements[i]->exec();
+    }
+  }
+
+  IoTScenario() {
+    // Задаём стандартные бинарные операторы.
+    // 1 - наименьший приоритет.
+    BinopPrecedence['<'] = 10;
+    BinopPrecedence['>'] = 10;
+    BinopPrecedence['+'] = 20;
+    BinopPrecedence['-'] = 20;
+    BinopPrecedence['*'] = 40;  
+    BinopPrecedence['='] = 60;  // highest.
+  }
+  
+  ~IoTScenario() {}
+};
 
 
 //===----------------------------------------------------------------------===//
 // Main driver code (Код основной программы)
 //===----------------------------------------------------------------------===//
 
-std::vector<ExprAST*> ScenarioElements;  // корневые элементы дерава   
-
-void loadScenario() {
-  if (myfile.is_open()) {
-    getNextToken();
-    while ( myfile ) {
-      switch (CurTok) {
-        case tok_eof:    return;
-        //case ';':        getNextToken(); break;  // игнорируем верхнеуровневые точки с запятой.
-        case tok_if:     ScenarioElements.push_back(ParseExpression()); break;
-        default:         getNextToken(); break;
-      }
-    }
-    myfile.close();
-  }
-}
-
+IoTScenario iotScen;
 
 int main() {
-  // Задаём стандартные бинарные операторы.
-  // 1 - наименьший приоритет.
-  BinopPrecedence['<'] = 10;
-  BinopPrecedence['>'] = 10;
-  BinopPrecedence['+'] = 20;
-  BinopPrecedence['-'] = 20;
-  BinopPrecedence['*'] = 40;  
-  BinopPrecedence['='] = 60;  // highest.
   
-  loadScenario();
+  iotScen.loadScenario("d:\\IoTScenario\\scenario.txt");
 
-  for (unsigned int i = 0; i < ScenarioElements.size(); i++) {
-    if (ScenarioElements[i]) ScenarioElements[i]->exec();
-  }
+  iotScen.ExecScenario();
+
+  
 
   return 0;
 }
